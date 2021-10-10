2ed38e7c93bd06c2226629e7f1dd892b67b07e63
Rushikesh <rushikesh.22010354@viit.ac.in>
2021-10-10 21:16:10 +0530

    Background and other things added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import './App.css';
 import axios from "axios";
+
 function App() {
-  return <h6>Welcome to weather app</h6>;
+  return (
+    <div class="app">
+      <div class="Todays_weather">
+        <div class="temp">16 &deg;</div>
+        <div class="city_datetime">
+          <span class="city">London</span>
+          <span class="datetime">06:09-Monday 9 sep'19</span>
+        </div>
+        <div class="weather">
+          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather icon" class="w-icon" height="50px" width="50px"></img>
+          <span>Cloudy</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
