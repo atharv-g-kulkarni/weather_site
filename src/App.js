573a85a52f4c266297bf61c04601aef278b27dea
Rushikesh <rushikesh.22010354@viit.ac.in>
2021-10-10 19:19:41 +0530

    Backgrond and other info added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import './App.css';
+
 function App() {
-  return <h6>Welcome to weather app</h6>;
+  return (
+    <div class="app">
+      <div class="Todays_weather">
+        <div class="temp">16 &deg;</div>
+        <div class="city_datetime">
+          <span class="city">London</span>
+          <span class="datetime">06:09-Monday 9 sep'19</span>
+        </div>
+        <div class="weather">
+          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather icon" class="w-icon" height="50px" width="50px"></img>
+          <span>Cloudy</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
